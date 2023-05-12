372bf9f8b8d15591703c1f6310f58adcc187755c
SBX01 <55763230+SBX01@users.noreply.github.com>
2023-05-12 13:57:17 -0400

    Update src image

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Navbar from './components/Navs/Menu-nav'
 class App extends Component{
   state = {
     productos:[
-      { name:'Tomates', price: 1500, img:'/productos/tomate.jpg'},
-      { name:'Arvejas', price: 2500, img:'/productos/arveja.jpg'},
-      { name:'Lechugas', price: 500, img:'/productos/lechuga.jpg'},
+      { name:'Tomates', price: 1500, img:'https://www.quironsalud.es/idcsalud-client/cm/images?locale=es_ES&idMmedia=2299323'},
+      { name:'Arvejas', price: 2500, img:'https://wongfood.vteximg.com.br/arquivos/ids/526925-750-750/154-1.jpg?v=637819234168930000'},
+      { name:'Lechugas', price: 500, img:'https://static.libertyprim.com/files/familles/romaine-large.jpg?1569271844'},
     ],
     carro : [],
     esCarroVisisble: false
